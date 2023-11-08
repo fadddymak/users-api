8da867a65923050863893901f80ae11004870b0a
fadddymak <135956181+fadddymak@users.noreply.github.com>
2023-11-08 18:48:53 +0100

    greet

--- routes/users/get_user.ts
+++ routes/users/get_user.ts
@@ -1,11 +1,12 @@
 import type { Route } from "@endpts/types";
+import {greet} from '../../lib/greet'
 
 export default {
   method: "GET",
   path: "/users",
   async handler(req) {
     const name = req.query.get("name") || "stranger";
-
+    greet()
     return new Response(`Hello! :)`);
   },
 } satisfies Route;
